af1c59b2905a45d13220b1a17d9b5b0f262f0761
davide cannerozzi <d.cannerozzi@gmail.com>
2025-11-12 13:29:05 +0100

    testing ga4 npm package

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import ReactGA from "react-ga4";
 
+ReactGA.initialize("G-BVPY2HHM6H");
+ReactGA.send("pageview");
 
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
